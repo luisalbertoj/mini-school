65284e48f6b068092150edad408aa85174e9381c
daniapraezss <daniapraez@hotmail.com>
2021-08-29 21:57:15 -0500

    back

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,30 @@
 const User = require("../models/user");
 
-const register = async (req, res) => { };
-const list = async (req, res) => { };
+const register = async (req, res) => {
+    let validId = mongoose.Types.ObjectId.isValid(req.body.userId);
+    if (!validId) return res.status(400).send("Invalid matter");
+    if (!req.body.name || !req.body.email || !req.body.password ||
+        !req.body.roleId)
+        return res.status(400).send('Incomplete data');
+    const user = new User({
+        name: req.body.titulo,
+        email: req.body.quantity,
+        password: req.body.dateIni,
+        roleId: req.body.roleId,
+        dbStatus: true,
+    
+    });
+    const result = await user.save();
+    if(!result) return res.status(400).send('Failed at create user');
+    res.status(200).send({message: 'User created', data: course});
+ };
+const list = async (req, res) => { 
+    let users = await User.find({ name: new RegExp(req.params["name"], "i") })
+    .populate("roleId")
+    .exec();
+  if (!users || users.length === 0)
+    return res.status(400).send("No search results");
+  return res.status(200).send({ users });
+};
 
 module.exports = { register, list };
